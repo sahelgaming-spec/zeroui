a9863ecb5d3a42e342f8f2f736c717fa99361f03
mohammed-sahel <mohammedsahel010@gmail.com>
2026-05-06 12:12:52 +0530

    Add GA4 tracking script to root head

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -55,6 +55,17 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DJ093H0LW"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6DJ093H0LW');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
